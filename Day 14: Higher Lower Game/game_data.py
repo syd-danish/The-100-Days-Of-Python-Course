6030bb9e6f4dd7616e3ef6e1535bbd4737f6d4e6
Syed Danish <142252763+syd-danish@users.noreply.github.com>
2025-01-25 01:45:22 +0400

    Add files via upload

---
+++ Day 14: Higher Lower Game/game_data.py
@@ -0,0 +1,509 @@
+data= [
+    {
+        "name": "Ronaldo",
+        "place_of_birth": "Portugal",
+        "profession": "a Football Player",
+        "followers": 215
+    },
+    {
+        "name": "Taylor Swift",
+        "place_of_birth": "United States",
+        "profession": "a Singer",
+        "followers": 280
+    },
+    {
+        "name": "Elon Musk",
+        "place_of_birth": "South Africa",
+        "profession": "an Entrepreneur",
+        "followers": 150
+    },
+    {
+        "name": "Emma Watson",
+        "place_of_birth": "United Kingdom",
+        "profession": "an Actress",
+        "followers": 100
+    },
+    {
+        "name": "Virat Kohli",
+        "place_of_birth": "India",
+        "profession": "a Cricketer",
+        "followers": 250
+    },
+    {
+        "name": "Lionel Messi",
+        "place_of_birth": "Argentina",
+        "profession": "a Football Player",
+        "followers": 230
+    },
+    {
+        "name": "Rihanna",
+        "place_of_birth": "Barbados",
+        "profession": "a Singer",
+        "followers": 140
+    },
+    {
+        "name": "Barack Obama",
+        "place_of_birth": "United States",
+        "profession": "a Politician",
+        "followers": 133
+    },
+    {
+        "name": "Roger Federer",
+        "place_of_birth": "Switzerland",
+        "profession": "a Tennis Player",
+        "followers": 95
+    },
+    {
+        "name": "Ariana Grande",
+        "place_of_birth": "United States",
+        "profession": "a Singer",
+        "followers": 220
+    },
+    {
+        "name": "Usain Bolt",
+        "place_of_birth": "Jamaica",
+        "profession": "an Athlete",
+        "followers": 65
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "place_of_birth": "Portugal",
+        "profession": "a Football Player",
+        "followers": 600
+    },
+    {
+        "name": "Mark Zuckerberg",
+        "place_of_birth": "United States",
+        "profession": "an Entrepreneur",
+        "followers": 45
+    },
+    {
+        "name": "Meryl Streep",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 10
+    },
+    {
+        "name": "Sachin Tendulkar",
+        "place_of_birth": "India",
+        "profession": "a Cricketer",
+        "followers": 100
+    },
+    {
+        "name": "Serena Williams",
+        "place_of_birth": "United States",
+        "profession": "a Tennis Player",
+        "followers": 120
+    },
+    {
+        "name": "Beyoncé",
+        "place_of_birth": "United States",
+        "profession": "a Singer",
+        "followers": 240
+    },
+    {
+        "name": "Albert Einstein",
+        "place_of_birth": "Germany",
+        "profession": "a Physicist",
+        "followers": 15
+    },
+    {
+        "name": "Malala Yousafzai",
+        "place_of_birth": "Pakistan",
+        "profession": "an Activist",
+        "followers": 20
+    },
+    {
+        "name": "Leonardo DiCaprio",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 55
+    },
+    {
+        "name": "Pele",
+        "place_of_birth": "Brazil",
+        "profession": "Football Player",
+        "followers": 45
+    },
+    {
+        "name": "Oprah Winfrey",
+        "place_of_birth": "United States",
+        "profession": "a Media Personality",
+        "followers": 90
+    },
+    {
+        "name": "Bill Gates",
+        "place_of_birth": "United States",
+        "profession": "an Entrepreneur",
+        "followers": 60
+    },
+    {
+        "name": "Adele",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Singer",
+        "followers": 150
+    },
+    {
+        "name": "Michael Jordan",
+        "place_of_birth": "United States",
+        "profession": "an Basketball Player",
+        "followers": 50
+    },
+    {
+        "name": "Stephen Hawking",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Physicist",
+        "followers": 12
+    },
+    {
+        "name": "Selena Gomez",
+        "place_of_birth": "United States",
+        "profession": "a Singer",
+        "followers": 310
+    },
+    {
+        "name": "David Beckham",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Football Player",
+        "followers": 95
+    },
+    {
+        "name": "Kylie Jenner",
+        "place_of_birth": "United States",
+        "profession": "a Businesswoman",
+        "followers": 400
+    },
+    {
+        "name": "LeBron James",
+        "place_of_birth": "United States",
+        "profession": "a Basketball Player",
+        "followers": 130
+    },
+    {
+        "name": "Shakira",
+        "place_of_birth": "Colombia",
+        "profession": "Singer",
+        "followers": 120
+    },
+    {
+        "name": "Nelson Mandela",
+        "place_of_birth": "South Africa",
+        "profession": "an Activist",
+        "followers": 25
+    },
+    {
+        "name": "Tom Cruise",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 35
+    },
+    {
+        "name": "Stephen Curry",
+        "place_of_birth": "United States",
+        "profession": "a Basketball Player",
+        "followers": 85
+    },
+    {
+        "name": "Maria Sharapova",
+        "place_of_birth": "Russia",
+        "profession": "a Tennis Player",
+        "followers": 75
+    },
+    {
+        "name": "Ed Sheeran",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Singer",
+        "followers": 200
+    },
+    {
+        "name": "Kobe Bryant",
+        "place_of_birth": "United States",
+        "profession": "a Basketball Player",
+        "followers": 95
+    },
+    {
+        "name": "Johnny Depp",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 45
+    },
+    {
+        "name": "Mahatma Gandhi",
+        "place_of_birth": "India",
+        "profession": "an Activist",
+        "followers": 10
+    },
+    {
+        "name": "Gal Gadot",
+        "place_of_birth": "Israhell",
+        "profession": "an Actress",
+        "followers": 60
+    },
+    {
+        "name": "Dwayne Johnson",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 350
+    },
+    {
+        "name": "J.K. Rowling",
+        "place_of_birth": "United Kingdom",
+        "profession": "an Author",
+        "followers": 30
+    },
+    {
+        "name": "Zinedine Zidane",
+        "place_of_birth": "France",
+        "profession": "a Football Player",
+        "followers": 50
+    },
+    {
+        "name": "Taylor Lautner",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 25
+    },
+    {
+        "name": "Justin Bieber",
+        "place_of_birth": "Canada",
+        "profession": "a Singer",
+        "followers": 300
+    },
+    {
+        "name": "Chris Hemsworth",
+        "place_of_birth": "Australia",
+        "profession": "an Actor",
+        "followers": 105
+    },
+    {
+        "name": "Greta Thunberg",
+        "place_of_birth": "Sweden",
+        "profession": "an Activist",
+        "followers": 18
+    },
+    {
+        "name": "Emma Stone",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 40
+    },
+    {
+        "name": "Andy Murray",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Tennis Player",
+        "followers": 35
+    },
+    {
+        "name": "Kim Kardashian",
+        "place_of_birth": "United States",
+        "profession": "a Media Personality",
+        "followers": 370
+    },
+    {
+        "name": "Vin Diesel",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 110
+    },
+    {
+        "name": "Simone Biles",
+        "place_of_birth": "United States",
+        "profession": "a Gymnast",
+        "followers": 50
+    },
+    {
+        "name": "Anne Hathaway",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 55
+    },
+    {
+        "name": "Keanu Reeves",
+        "place_of_birth": "Lebanon",
+        "profession": "an Actor",
+        "followers": 70
+    },
+    {
+        "name": "Cillian Murphy",
+        "place_of_birth": "Ireland",
+        "profession": "an Actor",
+        "followers": 20
+    },
+    {
+        "name": "Novak Djokovic",
+        "place_of_birth": "Serbia",
+        "profession": "a Tennis Player",
+        "followers": 85
+    },
+    {
+        "name": "Lewis Hamilton",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Racing Driver",
+        "followers": 110
+    },
+    {
+        "name": "Michelle Obama",
+        "place_of_birth": "United States",
+        "profession": "Author",
+        "followers": 100
+    },
+    {
+        "name": "Brad Pitt",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 80
+    },
+    {
+        "name": "Halle Berry",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 35
+    },
+    {
+        "name": "Rafael Nadal",
+        "place_of_birth": "Spain",
+        "profession": "a Tennis Player",
+        "followers": 100
+    },
+    {
+        "name": "Charlize Theron",
+        "place_of_birth": "South Africa",
+        "profession": "an Actress",
+        "followers": 25
+    },
+    {
+        "name": "Tom Hardy",
+        "place_of_birth": "United Kingdom",
+        "profession": "an Actor",
+        "followers": 30
+    },
+    {
+        "name": "Elton John",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Musician",
+        "followers": 50
+    },
+    {
+        "name": "Shawn Mendes",
+        "place_of_birth": "Canada",
+        "profession": "an Singer",
+        "followers": 180
+    },
+    {
+        "name": "Margot Robbie",
+        "place_of_birth": "Australia",
+        "profession": "an Actress",
+        "followers": 75
+    },
+    {
+        "name": "Tom Holland",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Actor",
+        "followers": 130
+    },
+    {
+        "name": "Zendaya",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 120
+    },
+    {
+        "name": "Hugh Jackman",
+        "place_of_birth": "Australia",
+        "profession": "a Actor",
+        "followers": 90
+    },
+    {
+        "name": "Lady Gaga",
+        "place_of_birth": "United States",
+        "profession": "a Singer",
+        "followers": 160
+    },
+    {
+        "name": "Chris Evans",
+        "place_of_birth": "United States",
+        "profession": "an Actor",
+        "followers": 110
+    },
+    {
+        "name": "Prince Harry",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Royal",
+        "followers": 60
+    },
+    {
+        "name": "Mila Kunis",
+        "place_of_birth": "Ukraine",
+        "profession": "an Actress",
+        "followers": 50
+    },
+    {
+        "name": "Ryan Reynolds",
+        "place_of_birth": "Canada",
+        "profession": "a Actor",
+        "followers": 125
+    },
+    {
+        "name": "Blake Lively",
+        "place_of_birth": "United States",
+        "profession": "an Actress",
+        "followers": 45
+    },
+    {
+        "name": "Galileo Galilei",
+        "place_of_birth": "Italy",
+        "profession": "an Astronomer",
+        "followers": 5
+    },
+    {
+        "name": "Isaac Newton",
+        "place_of_birth": "United Kingdom",
+        "profession": "a Physicist",
+        "followers": 10
+    },
+    {
+        "name": "Alexander Graham Bell",
+        "place_of_birth": "Scotland",
+        "profession": "an Inventor",
+        "followers": 7
+    },
+    {
+        "name": "Florence Nightingale",
+        "place_of_birth": "Italy",
+        "profession": "a Nurse",
+        "followers": 12
+    },
+    {
+        "name": "Jane Austen",
+        "place_of_birth": "United Kingdom",
+        "profession": "an Author",
+        "followers": 10
+    },
+    {
+        "name": "Mozart",
+        "place_of_birth": "Austria",
+        "profession": "a Composer",
+        "followers": 20
+    },
+    {
+        "name": "Vincent van Gogh",
+        "place_of_birth": "Netherlands",
+        "profession": "a Painter",
+        "followers": 30
+    },
+    {
+        "name": "Pablo Picasso",
+        "place_of_birth": "Spain",
+        "profession": "a Painter",
+        "followers": 25
+    },
+    {
+        "name": "Maya Angelou",
+        "place_of_birth": "United States",
+        "profession": "a Poet",
+        "followers": 20
+    }
+]
+
+
+

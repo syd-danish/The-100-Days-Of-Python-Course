2cf60e47191badf9a63187dcbdffce4b05243218
Syed Danish <142252763+syd-danish@users.noreply.github.com>
2025-02-01 13:16:17 +0400

    Create Day 1: BandNameGenerator.py

---
+++ Day 1: BandNameGenerator.py
@@ -0,0 +1,5 @@
+print("WELCOME TO THE BAND NAME GENERATOR\n")
+city_name=input("What is the name of the city you reside in?\n")
+animal=input("What is the name of your favorite animal?\n")
+
+print("Your Band Name is\n "+ animal +" of " + city_name)
